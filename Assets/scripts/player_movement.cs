11d8c388ed5db8e244aadd2f65509a2e15f76027
M451D4T <sidatmuhammad@gmail.com>
2021-12-31 01:44:22 +0000

    player movement added
    player can now move around the screen
    
    animation plays to match the character moving

---
+++ Assets/scripts/player_movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    
+    public float movespeed = 5f; //speed player moves should be changeable by items
+
+    public Rigidbody2D rb; //allows linking to rigid body of the player for physics
+    public Animator animator;
+    
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //inputs
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        //movment
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}
