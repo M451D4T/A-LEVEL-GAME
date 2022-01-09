58bd4835b48c5c5ba362a7bb6b348665e14dc8f3
M451D4T <sidatmuhammad@gmail.com>
2022-01-09 23:41:46 +0000

    added collision
    added collsions to player movement scripts

--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
-    
     public float movespeed = 5f; //speed player moves should be changeable by items
 
-    public Rigidbody2D rb; //allows linking to rigid body of the player for physics
+    //allows linking of componet to scripts
+    public Rigidbody2D rb; 
     public Animator animator;
-    
-    Vector2 movement;
+    public BoxCollider2D bc;
+    private RaycastHit2D hitx;
+    private RaycastHit2D hity;
+    private Vector2 movement;
 
     // Update is called once per frame
     void Update()
@@ -25,7 +27,15 @@ public class player_movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //movment
-        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+        //make sure we can move in x direction by casting a box there first if the box is null allows movement
+        hitx = Physics2D.BoxCast(transform.position, bc.size, 0, new Vector2(movement.x,0), movement.x*Time.fixedDeltaTime, LayerMask.GetMask("blocking","actor"));
+        //make sure we can move in y direction by casting a box there first if the box is null allows movement
+        hity = Physics2D.BoxCast(transform.position, bc.size, 0, new Vector2(0,movement.y), movement.y*Time.fixedDeltaTime, LayerMask.GetMask("blocking","actor"));
+        if (hity.collider==null && hitx.collider==null)
+        {
+            //movment happens
+            rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+        }
+
     }
 }
